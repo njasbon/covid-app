85fd3593a83186f37ff2ea769a6775987c87881c
Nicolle Jasbon <njasbon@gmail.com>
2020-07-16 11:26:44 -0400

    first commit for the covid app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import CountryStat from './CountryStat.js'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [filtered, setFiltered] = useState([])
+  const [countryStats, setCountryStats] = useState([])
+   
+  const showCountries = () => {
+    fetch("https://api.covid19api.com/countries")
+      .then(response => response.json())
+      .then(data => {
+        setCountries(data)
+        setFiltered(data)
+      })
+        
+  }
+
+
+  window.onload= showCountries
+
+  const covidCountries = countries.map(
+  (country, i) => <div key={i}> {country.Country}</div>
+  )
+
+  const updateCountries = () 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form" onSubmit={updateCountries}>
+        <label for="input">Search by Country </label>
+        <input type="text" id="country-search"/>
+        <input type="submit" value="Submit"/> 
+      </form>
+      {covidCountries}
     </div>
   );
 }
